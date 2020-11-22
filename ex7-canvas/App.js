da25fd13cf2d833b3b6a15c76a6cb48f6a94d977
kiyoung <popping2606@gmail.com>
2020-11-22 16:26:57 +0900

    ex7-canvas

---
+++ ex7-canvas/App.js
@@ -0,0 +1,69 @@
+import Block from './Block.js'
+import Ball from './Ball.js'
+
+class App {
+  constructor() {
+    this.canvas = document.createElement("canvas")
+    this.ctx = this.canvas.getContext("2d")
+
+    document.body.appendChild(this.canvas)
+
+    window.addEventListener("resize", this.resize.bind(this), false)
+    this.resize()
+
+    this.block = new Block(700, 100, 300, 450)
+
+    const $ballNumInput = document.querySelector('.ballNumInput')
+    $ballNumInput.addEventListener('keyup', ()=>{
+      this.setBallNum($ballNumInput.value)  
+    })
+    this.setBallNum(Math.random()*30+1)
+        
+
+    window.requestAnimationFrame(this.animate.bind(this))
+  }
+
+  resize() {
+    this.stageWidth = document.body.clientWidth
+    this.stageHeight = document.body.clientHeight
+
+    this.canvas.width = this.stageWidth * 2
+    this.canvas.height = this.stageHeight * 2
+    this.ctx.scale(2, 2)
+  }
+
+  animate(t) {
+    window.requestAnimationFrame(this.animate.bind(this))
+
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight)
+    
+    this.block.draw(this.ctx)
+    this.balls.forEach(i=>{
+      i.draw(this.ctx, this.stageWidth, this.stageHeight, this.block)
+    })
+  }
+
+  setBallNum(num){
+    num = parseInt(num)
+    if(num === undefined || isNaN(num) || num === '' || num <= 1){
+      num = 1
+    }else if(num > 30){
+      num = 30
+    }
+
+    const $ballNum = document.querySelector('.ballNum')
+    if(this.ballNum !== num){
+      this.ballNum = num
+      this.balls = []
+      if($ballNum) $ballNum.innerHTML = num
+      for(let i=0; i<this.ballNum; i++){
+        this.balls[i] = new Ball(this.stageWidth, this.stageHeight, Math.random()*60 +10, Math.random()*12 +1, this.block)
+      }
+      console.log(this.balls)
+    }
+  }
+} // App end
+
+window.onload = () => {
+  new App()
+}
---
+++ ex7-canvas/Ball.js
@@ -0,0 +1,87 @@
+export default class Ball {
+  constructor(stageWidth, stageHeight, radius, speed, block) {
+    this.radius = radius
+    this.vx = speed
+    this.vy = speed
+    this.vc = 1
+
+    const diameter = this.radius * 2
+    this.x = diameter + (Math.random() * stageWidth - diameter)
+    this.y = diameter + (Math.random() * stageHeight - diameter)
+
+    //블락 안에 공이 갇히지 않도록 초기화
+    const minX = block.x - this.radius
+    const maxX = block.maxX + this.radius
+    const minY = block.y - this.radius
+    const maxY = block.maxY + this.radius
+    while(this.x > minX && this.x < maxX && this.y > minY && this.y < maxY){
+      this.x = diameter + (Math.random() * stageWidth - diameter)
+      this.y = diameter + (Math.random() * stageHeight - diameter)
+    }
+
+    this.color = Math.floor(Math.random()*256)
+    this.fillStyle = `hsl(${this.color} 100% 50% / 0.7)`
+  }
+
+  draw(ctx, stageWidth, stageHeight, block) {
+    this.x += this.vx
+    this.y += this.vy
+    this.color += this.vc
+    if(this.color >= 255 || this.color <= 0) this.vc *= -1
+    this.fillStyle = `hsl(${this.color} 100% 50% / 0.7)`
+
+    this.bounceWindow(stageWidth, stageHeight)
+    this.bounceBlock(block)
+
+    ctx.fillStyle = this.fillStyle
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+  bounceWindow(stageWidth, stageHeight) {
+    const minX = this.radius
+    const maxX = stageWidth - this.radius
+    const minY = this.radius
+    const maxY = stageHeight - this.radius
+
+    if (this.x <= minX) {
+      if(this.vx <= 0) this.vx *= -1
+    }
+    if(this.x >= maxX){
+      if(this.vx > 0) this.vx *= -1
+    }
+    if (this.y <= minY) {
+      if(this.vy <= 0) this.vy *= -1
+    }
+    if(this.y >= maxY){
+      if(this.vy > 0) this.vy *= -1
+    }
+  }
+
+  bounceBlock(block){
+    const minX = block.x - this.radius
+    const maxX = block.maxX + this.radius
+    const minY = block.y - this.radius
+    const maxY = block.maxY + this.radius
+
+    if(this.x > minX && this.x < maxX && this.y > minY && this.y < maxY){
+      const x1 = Math.abs(minX - this.x)
+      const x2 = Math.abs(maxX - this.x)
+      const y1 = Math.abs(minY - this.y)
+      const y2 = Math.abs(maxY - this.y)
+      const min1 = Math.min(x1, x2)
+      const min2 = Math.min(y1, y2)
+      const min = Math.min(min1, min2)
+
+      if(min === min1){
+        this.vx *= -1
+        this.x += this.vx
+      }
+      if(min === min2){
+        this.vy *= -1
+        this.y += this.vy
+      }
+    }
+  }
+} //Ball end
